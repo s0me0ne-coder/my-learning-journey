043334d284317c3015cbb72de899dc0a27ebe26d
s0me0ne-coder <tolgaucar44@gmail.com>
2023-10-15 21:55:08 +0300

    fix remove_comments :)

--- remove-comments.c
+++ remove-comments.c
@@ -3,47 +3,87 @@
 int getline2(char s[], int lim);
 void clear(char s[], int lim);
 
+enum STATE {
+    normal = 0,
+    comment = 1,
+    slash_comment = 2,
+    string = 3,
+    char_string = 4,
+};
+
 int main()
 {
     /* some random comment */
-    int backslash = 0;
+    enum STATE state = normal;
     char s[10000];
-    int empty_lines = 0;
-    int in_string = 0;
-    int in_char = 0;
-    int in_comment = 0;
-    int in_slash_comment = 0;
     for(int i = getline2(s,10000); i != -1 ; i = getline2(s,10000))
     {
         for (int x = 0; x <= i; ++x) {
-            if(s[x] == '"') {
-                in_string = 1;
-            } else if(s[x] == '\'') {
-                in_char = 1;
-            } else if(s[x] == '/' && x + 1 <= i &&  s[x+1] == '*') {
-                in_comment = 1;
-                x += 1;
-                continue;
-            } else if(s[x] == '/' && x + 1 <= i && s[x+1] == '/') {
-                in_slash_comment = 1;
-                x += 1;
-                continue;
+            if(state == normal) {
+                // normal -> string/char
+                if(s[x] == '"') {
+                    state = string;
+                    putchar(s[x]);
+                }
+                else if(s[x] == '\'') {
+                    state = char_string;
+                    putchar(s[x]);
+                }
+                // normal -> comment
+                else if(s[x] == '/' && x + 1 <= i && s[x+1] == '*')
+                {
+                    state = comment;
+                    x += 1;
+                    continue;
+                }
+                // normal -> slash comment
+                else if(s[x] == '/' && x + 1 <= i && s[x+1] == '/') {
+                    state = slash_comment;
+                    x += 1;
+                    continue;
+                }
+                /* any other character */
+                // any other character
+                else {
+                    putchar(s[x]);
+                }
             }
-
-            if(in_slash_comment == 1 && s[x] == '\n') {
-                in_slash_comment = 0;
-                putchar(s[x]);
-                continue;
+            else if(state == string || state == char_string)
+            {
+                /* backslash ignorer */
+                if(s[x] == '\\' && x + 2 <= i) {
+                    putchar(s[x]);
+                    x += 1;
+                    putchar(s[x]);
+                    continue;
+                }
+                // char_string -> normal
+                else if(s[x] == '\'' && state == char_string) {
+                    state = normal;
+                    putchar(s[x]);
+                }
+                /* string -> normal */
+                else if(s[x] == '"' && state == string){
+                    state = normal;
+                    putchar(s[x]);
+                }
+                else putchar(s[x]);
             }
-
-            if(in_comment == 0 && in_slash_comment == 0) {
-                putchar(s[x]);
-            }  else if(s[x] == '*' && x + 1 <= i && s[x+1] == '/') {
-                in_comment = 0;
-                x += 1;
-                continue;
+            else if(state == comment)
+            {
+                if(s[x] == '*' && x + 1 <= i && s[x+1] == '/')
+                {
+                    state = normal;
+                    x += 1;
+                }
             }
-
+            else if(state == slash_comment)
+            {
+                if(s[x] == '\n') {
+                    state = normal;
+                    putchar(s[x]);
+                }
+            }
         }
     }
     return 0;
